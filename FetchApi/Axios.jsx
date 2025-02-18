37384b29e8d0921adec53f43bf11bc2016c19696
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2025-02-18 18:52:24 +0530

    Create Axios.jsx

---
+++ FetchApi/Axios.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'
+
+const Api = "https://jsonplaceholder.typicode.com/posts";
+
+function App() {
+  const [apiData, setApiData] = useState([]);
+
+  async function CallApi() {
+    try {
+      const response = await axios.get(Api);
+      setApiData(response.data);
+
+    } catch(error) {
+               console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    CallApi(); // Component mount hone pe pehli baar API call karna better hai
+
+    const interval = setInterval(() => {
+      CallApi()
+    }, 5000); // 5 second ka interval rakha
+
+    // return () => clearInterval(interval); // Cleanup function to avoid memory leak
+  }, []);
+
+  return (
+    <div>
+      {apiData.map((currentData) => (
+        <div key={currentData.id}>
+          <h1>{currentData.id}</h1>
+          <li>{currentData.title}</li>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
